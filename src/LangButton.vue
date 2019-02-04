d910906b68faee1c67c2c9a7a8a855c34f3a12db
Sundagy <mblinenkov@yandex.ru>
2019-02-04 16:03:43 +0300

    Add language bar

---
+++ src/LangButton.vue
@@ -0,0 +1,62 @@
+<template>
+  <button :class="buttonClasses" @click="$emit('click')">
+    {{ text.toUpperCase() }}
+  </button>
+</template>
+
+<script type="ts">
+  import Vue from 'vue'
+
+  export default Vue.extend({
+    name: "LangButton",
+    props: {
+      text: {
+        type: String,
+        require: true,
+      },
+      select: {
+        type: Boolean,
+        default: false,
+      },
+      filled: {
+        type: Boolean,
+        default: false,
+      },
+    },
+    data: () => ({}),
+    computed: {
+      buttonClasses() {
+        let result = [];
+        if (this.select) result.push('select');
+        if (this.filled) result.push('filled');
+        return result.join(' ')
+      },
+    },
+  })
+</script>
+
+<style scoped lang="scss">
+
+  button {
+    background: white;
+    border: 1.1px solid #979797;
+    border-radius: 3px;
+    color: #777777;
+    margin-right: 7px;
+    margin-top: 6px;
+    font-size: 14px;
+    padding: 2px 11px 0px 11px;
+    cursor: pointer;
+    &.filled {
+      border-color: #ea7e00;
+    }
+    &.select {
+      border-color: #5ac1f0;
+      color: #5ac1f0;
+    }
+    &:active, &:focus {
+      outline: none;
+    }
+  }
+
+</style>
---
+++ src/LangsBar.vue
@@ -0,0 +1,48 @@
+
+<template>
+  <div class="langs-bar">
+
+    <LangButton v-for="(val) in list"
+                @click="onSelect(val)"
+                :filled="!!occupied[val]"
+                :select="val === value"
+                :key="val"
+                :text="val"/>
+
+  </div>
+</template>
+
+<script type="ts">
+  import Vue from 'vue'
+  import LangButton from './LangButton'
+
+  export default Vue.extend({
+    components: {LangButton},
+    props: {
+      list: {
+        type: Array,
+        default: () => ['en', 'ru', 'fr', 'ptbr'],
+      },
+      occupied: {
+        type: Object,
+        default: () => {{}},
+      },
+      value: {
+        type: String,
+        default: 'en',
+      }
+    },
+    name: "LangsBar",
+    methods: {
+      onSelect(value){
+        this.$emit('change', value);
+      }
+    }
+  })
+</script>
+
+<style scoped lang="scss">
+  .langs-bar {
+
+  }
+</style>
