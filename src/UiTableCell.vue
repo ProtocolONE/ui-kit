9ff074b8495151383ea007ed5f99bccf87b25915
Andrey Solodovnikov <a.solodovnikov@office.ngs.ru>
2019-02-22 12:11:21 +0300

    adds UiTable components

---
+++ src/UiTableCell.vue
@@ -0,0 +1,40 @@
+<template>
+<div
+  class="ui-table-cell"
+  :class="{'_head': isHead}"
+>
+  <slot />
+</div>
+</template>
+
+<script>
+export default {
+  name: 'UiTableRow',
+
+  data() {
+    return {
+      isHead: this.$parent.isHead || false,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.ui-table-cell {
+  display: table-cell;
+  padding: 12px 0 16px 14px;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.08);
+  color: #0c2441;
+
+  &:first-child {
+    padding-left: 26px;
+  }
+
+  &._head {
+    padding-top: 10px;
+    padding-bottom: 10px;
+    background: #f6f6f6;
+    color: #b1b1b1;
+  }
+}
+</style>
