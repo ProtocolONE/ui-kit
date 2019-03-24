ca75c2fa08f25e4d7ec899f76e8403455e59bdbd
Andrey Solodovnikov <a.solodovnikov@office.ngs.ru>
2019-03-24 23:39:52 +0300

    adds error text for UiTextarea

--- src/UiTextarea.vue
+++ src/UiTextarea.vue
@@ -1,14 +1,21 @@
 <template>
-<div class="textarea">
+<div class="ui-textarea">
   <label :class="['label', { '_required': required }]">
     {{ label }}
   </label>
+  <span
+    v-if="isVisibleError"
+    class="error"
+    :title="errorText"
+  >
+    {{ errorText }}
+  </span>
   <textarea
-    v-text="value"
     :disabled="disabled"
-    :class="['input', { '_bordered': isBordered, '_disabled': disabled }]"
+    :class="['input', { '_bordered': isBordered, '_disabled': disabled, '_error': isVisibleError }]"
     :style="{ height: `${countLines * 32}px`}"
     @input="$emit('input', $event.target.value)"
+    v-text="value"
   />
 </div>
 </template>
@@ -44,14 +51,36 @@ export default {
       required: true,
       type: String,
     },
+    errorText: {
+      default: '',
+      type: String,
+    },
+    hasError: {
+      default: false,
+      type: Boolean,
+    },
   },
+  
+  computed: {
+    /**
+     * Error is visible if it exists and error text isn't empty
+     * @return {boolean}
+     */
+    isVisibleError() {
+      return !!(this.hasError && this.errorText);
+    },
+  }
 }
 </script>
 
 <style scoped lang="scss">
-.textarea {
+$secondary-input-size: 14px;
+$error-input-color: #ff6f6f;
+.ui-textarea {
+  position: relative;
   padding: 24px 0;
 }
+
 .label {
   color: #b1b1b1;
   margin: 0;
@@ -89,5 +118,16 @@ export default {
     color: #b1b1b1;
     pointer-events: none;
   }
+
+  &._error {
+    border-bottom-color: $error-input-color;
+  }
+}
+.error {
+  bottom: 0;
+  color: $error-input-color;
+  display: block;
+  font-size: $secondary-input-size;
+  position: absolute;
 }
 </style>
