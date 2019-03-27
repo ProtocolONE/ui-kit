c3412593a62dcc6e6481ecd3b9452889e995d734
Pavel Kokovin <pavel.kokovin@protocol.one>
2019-03-27 16:00:49 +0700

    Add possible remove padding fron UiTip

--- src/Tip.vue
+++ src/Tip.vue
@@ -28,6 +28,10 @@ export default {
       default: false,
       type: Boolean,
     },
+    withoutPadding: {
+      default: false,
+      type: Boolean,
+    },
   },
   computed: {
     tipClasses() {
@@ -35,6 +39,7 @@ export default {
         'simple-tip',
         `_${this.color}`,
         this.visible ? '_show' : '',
+        this.withoutPadding ? '_without-padding' : '',
       ];
     },
   },
@@ -42,14 +47,14 @@ export default {
 </script>
 
 <style scoped lang="scss">
-@mixin simple-tip($background: #fff, $padding: 32px) {
+@mixin simple-tip($background: #fff) {
   right: 0;
   top: 50%;
   transform: translate(calc(100% + 16px), -50%);
   position: absolute;
   border-radius: 2px;
   box-shadow: 0px 4px 16px rgba(0, 0, 0, 0.15);
-  padding: $padding;
+  padding: 32px;
   background-color: $background;
   pointer-events: none;
   opacity: 0;
@@ -65,6 +70,10 @@ export default {
     transform: scaleY(1.6);
     font-size: 12px;
     color: $background;
+  }
+
+  &._without-padding {
+    padding: 0;
   }
 }
 
